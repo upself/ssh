ce3b1f0fe1a4ca7d632799d47323eadd94ec4372
liuhaodl <dllhao@cn.ibm.com>
2016-01-11 15:14:39 +0800

    remove system.out in action

--- myArtId/src/main/java/myArtId/HelloWorldAction.java
+++ myArtId/src/main/java/myArtId/HelloWorldAction.java
@@ -6,12 +6,10 @@ public class HelloWorldAction extends ActionSupport {
 	
 	@Override
 	public String execute() throws Exception {
-		System.out.println("this is in execute method.");
 		return ActionSupport.SUCCESS;
 	}
 	
 	public void validate() {
-		System.out.println("this is in validation of helloWorld action");
 		super.validate();
 	}
 }
