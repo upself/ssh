888ec54f9232c7c3a99e9103d8b3f862c592b966
liuhao <liuhao@IBM931-PC01WYGQ>
2016-04-20 14:26:41 +0800

    add page

--- springMVC/src/com/liuhao/spring/HomeController.java
+++ springMVC/src/com/liuhao/spring/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value="/")
+	@RequestMapping(value="/homeAction")
 	public String home(){
 		System.out.println("do some handling");
-		return "/home.jsp";
+		return "homePage";
 	}
 
 }
